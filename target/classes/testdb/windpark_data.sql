dcb336b56368280d2dd92301ecc327dc5b7b78f8
Thomas Micheler <thomas.micheler@gmail.com>
2021-04-10 12:06:41 +0200

    repository cleaned

--- target/classes/testdb/windpark_data.sql
+++
@@ -1,2 +0,0 @@
-
-insert into T_WINDPARK (NUMBER, NAME) values ('999999999', 'Windpark Maxi');
--- target/classes/testdb/windpark_schema.sql
+++
@@ -1,6 +0,0 @@
-drop table T_WINDPARK if exists;
-
-create table T_WINDPARK (ID bigint identity primary key, NUMBER varchar(9),
-                        NAME varchar(50) not null, BALANCE decimal(8,2), unique(NUMBER));
-                        
-ALTER TABLE T_WINDPARK ALTER COLUMN BALANCE SET DEFAULT 0.0;
